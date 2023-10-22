0278a565a506bdb3a49951e878712043ab76aa99
mufaddal <mufaddalaurangabadi@gmail.com>
2023-10-22 19:51:51 +0530

    meta info

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -34,10 +34,10 @@ export default class MyDocument extends Document {
           {/* Meta */}
           <meta
             name="description"
-            content="The starting point for your next project with Minimal UI Kit, built on the newest version of Material-UI ©, ready to be customized to your style"
+            content="Welcome to Medirecruiters, a one of a kind medical consultancy."
           />
-          <meta name="keywords" content="react,material,kit,application,dashboard,admin,template" />
-          <meta name="author" content="Minimal UI Kit" />
+          <meta name="keywords" content="resources, doctors, help, healthcare, nursing staff, medical staff" />
+          <meta name="author" content="Medirecruiters" />
         </Head>
 
         <body>
